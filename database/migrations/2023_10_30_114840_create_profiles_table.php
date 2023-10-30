57edadc8805302fa739dfb83f63cd59cf07f656c
davidchinyio <2007587.swansea.ac.uk>
2023-10-30 13:09:49 +0000

    Added fields to the profiles migration table

--- database/migrations/2023_10_30_114840_create_profiles_table.php
+++ database/migrations/2023_10_30_114840_create_profiles_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('first name');
+            $table->string('last name');
+            $table->string('email address');
             $table->timestamps();
         });
     }
