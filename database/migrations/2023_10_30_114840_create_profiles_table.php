a48d5efca5c1841f9288f1fb5301d831078cee5d
davidchinyio <2007587.swansea.ac.uk>
2023-11-02 10:42:59 +0000

    Added initial record to database and created Profile seeder class

--- database/migrations/2023_10_30_114840_create_profiles_table.php
+++ database/migrations/2023_10_30_114840_create_profiles_table.php
@@ -13,9 +13,8 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->string('first name');
-            $table->string('last name');
-            $table->string('email address');
+            $table->string('name'); 
+            $table->string('email');
             $table->timestamps();
         });
     }
